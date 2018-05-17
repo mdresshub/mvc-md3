5b1eaa58175a5f0bb02c70f0b8f7fab157b1c262
mdresshub <michdressler@gmail.com>
2018-05-17 23:37:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdressler
+ * Date: 17.05.2018
+ * Time: 00:27
+ */
+
+spl_autoload_register(function ($classname) {
+    if (substr($classname, 0, 4) !== 'Mvc\\') {
+        return;
+    }
+
+    $filename = __DIR__.DIRECTORY_SEPARATOR.str_replace('\\', DIRECTORY_SEPARATOR, substr($classname, 4)).'.php';
+
+    if (file_exists($filename)) {
+        include $filename;
+    }
+});
