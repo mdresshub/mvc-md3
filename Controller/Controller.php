6c9aa0f00bd01884a7da491ddbc8619709841dfc
mdresshub <michdressler@gmail.com>
2018-05-18 22:30:07 +0200

    added controller classes

---
+++ Controller/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdressler
+ * Date: 18.05.2018
+ * Time: 22:14
+ */
+
+namespace Mvc\Controller;
+
+
+interface Controller
+{
+    public function indexAction();
+}
---
+++ Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mdressler
+ * Date: 18.05.2018
+ * Time: 22:10
+ */
+
+namespace Mvc\Controller;
+
+
+class IndexController implements Controller
+{
+    public function indexAction() {
+        echo 'Hello World!';
+    }
+}
