9da0af30ea607be689bb62c546b2e0a77925d170
mdresshub <michdressler@gmail.com>
2018-05-18 22:05:59 +0200

    added controller routing, class loading, method call, exception throws

--- index.php
+++ index.php
@@ -17,3 +17,35 @@ spl_autoload_register(function ($classname) {
         include $filename;
     }
 });
+
+// get the requested url
+$url = (isset($_GET['_url']) ? $_GET['_url'] : '');
+$urlParts = explode('/', $url);
+
+// build the controller class
+$controllerName = (isset($urlParts[0]) && $urlParts[0] ? $urlParts[0] : 'index');
+$controllerClassName = '\\Mvc\\Controller\\'.ucfirst($controllerName).'Controller';
+
+// build the action method
+$actionName = (isset($urlParts[1]) && $urlParts[1] ? $urlParts[1] : 'index');
+$actionMethodName = ucfirst($actionName).'Action';
+
+try {
+    if (!class_exists($controllerClassName)) {
+        throw new \Mvc\Library\NotFoundException();
+    }
+
+    $controller = new $controllerClassName;
+
+    if (!$controller instanceof \Mvc\Controller\Controller || !method_exists($controller, $actionMethodName)) {
+        throw new \Mvc\Library\NotFoundException();
+    }
+
+    $controller->$actionMethodName();
+} catch (\Mvc\Library\NotFoundException $e) {
+    http_response_code(404);
+    echo 'Page not found: '.$controllerClassName.'::'.$actionMethodName;
+} catch (\Exception $e) {
+    http_response_code(500);
+    echo 'Exception: '.$e->getMessage().' '.$e->getTraceAsString();
+}
